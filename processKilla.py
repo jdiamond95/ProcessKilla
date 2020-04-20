8206242b115362ebad2e21a4dbb45c17545a6c38
Jac Diamond <jac.diamond@hotmail.com>
2020-04-20 19:29:30 +1000

    Basic pslist with verbosity

--- processKilla.py
+++ processKilla.py
@@ -5,51 +5,59 @@ from tabulate import tabulate
 approvedFlags = ["-h", "-l", "-v"]
 
 def main():
-	
-	if len(sys.argv) == 1:
-		print("usage: python processKilla.py [-h help] [-l List Processes] [-n Process Name string] [-v Verbose]")
-	elif len(sys.argv) > 1:
+
+	#Check whether any flags are incorrectly used - recommend help if so
+	if len(sys.argv) > 1:
 		if unapprovedFlags():
 			print("Unapproved flags used - use \"python processKilla.py -h\" for help")
 			return(0)
 
-	elif sys.argv[1] == "-h":
-		print("Help im lost")
+	#No flags, help
+	if len(sys.argv) == 1:
+		print("usage: python processKilla.py [-h help] [-l List Processes] [-n Process Name string] [-v Verbose]")
+	
+	#Help flag
+	elif "-h" in sys.argv:
+		print("usage: python processKilla.py [-h help] [-l List Processes] [-n Process Name string] [-v Verbose]")
 
 	elif "-l" in sys.argv:
-		table = []
-		if "-v" in sys.argv:
-			table.append(["Process ID", "Process Name"])
-			for proc in psutil.process_iter():
-				try:	
-										
-					processName = proc.name()
-					processID = proc.pid
-					table.append([processID, processName])
-					print(dir(proc	))
-				except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-					pass
-
-		else:
-			for proc in psutil.process_iter():
-				try:
-					processName = proc.name()
-					processID = proc.pid
-					table.append([processID, processName])
-				except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-					pass
-
-		print(tabulate(table))
-
+		printProcessList()
 
 
 def unapprovedFlags():
 	result = False
-	for flag in sys.argv:
+	for flag in sys.argv[1:]:
 		if not flag in approvedFlags:
 			result = True
 	return result
 
+
+def printProcessList():
+	table = []
+
+	#Verbose flag used, provide more process info
+	if "-v" in sys.argv:
+		table.append(["Process ID", "Process Name"])
+		for proc in psutil.process_iter():
+			try:						
+				processName = proc.name()
+				processID = proc.pid
+				# threads = proc.threads()
+				exe = proc.exe()
+				table.append([processID, processName, exe])
+			except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+				pass
+
+	else:
+		for proc in psutil.process_iter():
+			try:
+				processName = proc.name()
+				processID = proc.pid
+				table.append([processID, processName])
+			except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+				pass
+
+	print(tabulate(table))
 
 
 if __name__ == "__main__":
